5578db4ddccbb509e0626fcc37970a4142fbf022
Rick Belson <belsonrj@aol.com>
2020-09-19 21:26:19 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 		return (
 			<div className="App">
 				<CatComponent />
-				<p><GraceHopperQuoteComponent /></p>
+				<GraceHopperQuoteComponent />
 				<MouseComponent />
 			</div>
 		);
